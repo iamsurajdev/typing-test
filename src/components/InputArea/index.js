9eb896f2f0daced4e0785e03d5e0e865e8d92b7d
iamsurajdev <surajbiswas367@gmail.com>
2021-02-14 15:19:34 -0800

    add some components

---
+++ src/components/InputArea/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const InputArea = () => {
+  return <div>InputArea</div>;
+};
+
+export default InputArea;
---
+++ src/components/ShowText/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ShowText = () => {
+  return <div>ShowText</div>;
+};
+
+export default ShowText;
