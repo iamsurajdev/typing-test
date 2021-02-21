588c5b97194d4b97ad2febf05a5f0ed79b9fd9c0
iamsurajdev <surajbiswas367@gmail.com>
2021-02-21 14:26:55 -0800

    add header and boiler plate react component to every new file

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Footer = () => {
+const Header = () => {
   return (
     <div>
-      <h1>Footer</h1>
+      <h1>Header</h1>
     </div>
   );
 };
 
-export default Footer;
+export default Header;
---
+++ src/components/TypingSppedInfo/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TypingSpeedInfo = () => {
+  return (
+    <div>
+      <h1>TypingSpeedInfo</h1>
+    </div>
+  );
+};
+
+export default TypingSpeedInfo;
