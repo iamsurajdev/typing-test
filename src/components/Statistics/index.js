281e7496890d3c3d763a9a174b49b39bec234167
iamsurajdev <surajbiswas367@gmail.com>
2021-02-23 20:28:52 -0800

    add all statistics items

--- src/components/Statistics/index.js
+++ src/components/Statistics/index.js
@@ -1,15 +1,16 @@
 import React from "react";
+import styles from "./style.module.css";
 import StatisticsItem from "../../util/UI/StatisticsItem";
 
 const Statistics = () => {
   return (
-    <div>
-      <StatisticsItem />
-      <StatisticsItem />
-      <StatisticsItem />
-      <StatisticsItem />
-      <StatisticsItem />
-      <StatisticsItem />
+    <div className={styles.main}>
+      <StatisticsItem name="Last Score" />
+      <StatisticsItem name="WPM" color="#01579B" />
+      <StatisticsItem name="CPM" color="#01579B" />
+      <StatisticsItem name="Errors" color="#a71627" />
+      <StatisticsItem name="Accuracy" color="#12b63f" />
+      <StatisticsItem name="Timer" color="#F50057" />
     </div>
   );
 };
