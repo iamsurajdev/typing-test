8c0408873872193da0a35c01de88ca3f02e9bdb1
iamsurajdev <surajbiswas367@gmail.com>
2021-02-24 01:00:50 -0800

    add all logics

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import Statistics from "./components/Statistics";
 import Tips from "./components/Tips";
 import TypingSpeedInfo from "./components/TypingSpeedInfo";
 
+let interval = null;
+
 const App = () => {
   const inputRef = useRef(null);
   const outputRef = useRef(null);
@@ -26,9 +28,83 @@ const App = () => {
   const [isError, setIsError] = useState(false);
   const [lastScore, setLastScore] = useState("0");
 
-  const handleStart = () => {
-    //
+  useEffect(() => {
+    const newQuote = random(quotesArray);
+    setQuote(newQuote);
+    setInput(newQuote.quote);
+  }, []);
+
+  const handleEnd = () => {
+    setEnded(true);
+    setStarted(false);
+    clearInterval(interval);
   };
+
+  const setTimer = () => {
+    const now = Date.now();
+    const seconds = now + duration * 1000;
+    interval = setInterval(() => {
+      const secondLeft = Math.round((seconds - Date.now()) / 1000);
+      setDuration(secondLeft);
+      if (secondLeft === 0) {
+        handleEnd();
+      }
+    }, 1000);
+  };
+
+  const handleStart = () => {
+    setStarted(true);
+    setEnded(false);
+    setInput(quote.quote);
+    inputRef.current.focus();
+    setTimer();
+  };
+
+  const handleKeyDown = (e) => {
+    e.preventDefault();
+    const { key } = e;
+    const quoteText = quote.quote;
+
+    if (key === quoteText.charAt(index)) {
+      setIndex(index + 1);
+      const currenChar = quoteText.substring(
+        index + 1,
+        index + quoteText.length
+      );
+      setInput(currenChar);
+      setCorrectIndex(correctIndex + 1);
+      setIsError(false);
+      outputRef.current.innerHTML += key;
+    } else {
+      if (allowedKeys.includes(key)) {
+        setErrorIndex(errorIndex + 1);
+        setIsError(true);
+        outputRef.current.innerHTML += `<span class="text-danger">${key}</span>`;
+      }
+    }
+
+    const timeRemains = ((60 - duration) / 60).toFixed(2);
+    const _accuracy = Math.floor(((index - errorIndex) / index) * 100);
+    const _wpm = Math.round(correctIndex / 5 / timeRemains);
+
+    if (index > 5) {
+      setAccuracy(_accuracy);
+      setCpm(correctIndex);
+      setWpm(_wpm);
+    }
+
+    if (index + 1 === quoteText.length || errorIndex > 50) {
+      handleEnd();
+    }
+  };
+
+  useEffect(() => {
+    if (ended) localStorage.setItem("wpm", wpm);
+  }, [ended, wpm]);
+  useEffect(() => {
+    const stroedScore = localStorage.getItem("wpm");
+    if (stroedScore) setLastScore(stroedScore);
+  }, []);
 
   return (
     <div>
@@ -65,8 +141,17 @@ const App = () => {
           <Tips />{" "}
         </div>
         <div className={styles.rightSideInContainer}>
-          <ShowText />
-          <InputArea state={input} setState={setInput} />
+          <ShowText
+            quote={quote}
+            started={started}
+            ended={ended}
+            isError={isError}
+            inputRef={inputRef}
+            input={input}
+            handleKeyDown={handleKeyDown}
+          />
+
+          <InputArea value={outputRef} />
           <TypingSpeedInfo />
         </div>
       </div>
