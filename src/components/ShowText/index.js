dd1d8cc497eeeef18bf8ec033595da7425b48e12
iamsurajdev <surajbiswas367@gmail.com>
2021-02-24 01:00:12 -0800

    Show text component update

--- src/components/ShowText/index.js
+++ src/components/ShowText/index.js
@@ -1,10 +1,40 @@
 import React from "react";
 import styles from "./style.module.css";
 
-const ShowText = ({ text = "Demo text" }) => {
+const ShowText = ({
+  quote,
+  started,
+  ended,
+  isError,
+  inputRef,
+  input,
+  handleKeyDown,
+}) => {
   return (
     <div className={styles.main}>
-      <span>{text}</span>
+      {ended ? (
+        <div className={styles.ended_div}>
+          <span>"{quote.quote}"</span>
+          <br />
+          <span className={styles.ended_div_author_span}>- {quote.author}</span>
+        </div>
+      ) : started ? (
+        <div
+          className={styles.started_div}
+          className={` quotes${started ? " active" : ""}${
+            isError ? " is-error" : ""
+          }`}
+          tabIndex="0"
+          onKeyDown={handleKeyDown}
+          ref={inputRef}
+        >
+          {input}
+        </div>
+      ) : (
+        <div className={styles.last_section_div} tabIndex="-1" ref={inputRef}>
+          {input}
+        </div>
+      )}
     </div>
   );
 };
