e6ba3dc64fd57adeb974ccf206d2703f5fe394ae
iamsurajdev <surajbiswas367@gmail.com>
2021-02-23 20:17:17 -0800

    add default atributes in StatisticsItem props

--- src/util/UI/StatisticsItem/index.js
+++ src/util/UI/StatisticsItem/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import styles from "./style.module.css";
 
-const StatisticsItem = ({ name, value, color = "#512DA8" }) => {
+const StatisticsItem = ({ name = "N/A", value = "N/A", color = "#512DA8" }) => {
   return (
     <div className={styles.main} style={{ border: `5px solid ${color}` }}>
-      <span className={styles.value}>value</span>
-      <span className={styles.name}>name</span>
+      <span className={styles.value}>{value}</span>
+      <span className={styles.name}>{name}</span>
     </div>
   );
 };
