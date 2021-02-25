ad0bcde1a13fa34bcd95c7d79ebf083491468949
iamsurajdev <surajbiswas367@gmail.com>
2021-02-24 21:08:19 -0800

    update Tips html

--- src/components/Tips/index.js
+++ src/components/Tips/index.js
@@ -1,9 +1,20 @@
 import React from "react";
+import styles from "./style.module.css";
 
 const Tips = () => {
   return (
-    <div>
-      <h1>Tips</h1>
+    <div className={styles.main}>
+      <p className={styles.heading}>Tips..!</p>
+      <ul className={styles.tips_container}>
+        <li className={styles.tips}>Focus on accuracy over speed</li>
+        <li className={styles.tips}>
+          Stop with the hunt and peck. Hand position is an important part of
+          typing.
+        </li>
+        <li className={styles.tips}>Stretch your hands, neck, and shoulders</li>
+        <li className={styles.tips}>Learn touch typing</li>
+        <li className={styles.tips}>Don,t stress </li>
+      </ul>
     </div>
   );
 };
